88f3aca18fe6076f02581ea4afe503b9ac46c875
Francisco de Assis <a124fr@gmail.com>
2021-06-24 16:31:02 -0300

    Retorno de dados em xml ou json por webservice

--- src/main/java/br/com/alura/gerenciador/servlet/EmpresaService.java
+++ src/main/java/br/com/alura/gerenciador/servlet/EmpresaService.java
@@ -1,7 +1,6 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -11,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
@@ -22,10 +22,26 @@ public class EmpresaService extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		List<Empresa> empresas = Banco.getEmpresas();
 		
-		Gson gson = new Gson();
-		String json = gson.toJson(empresas);
+		String valor = request.getHeader("Accept");
 		
-		response.setContentType("application/json");
-		response.getWriter().print(json);		
+		System.out.println(valor);
+		
+		if(valor.contains("xml")) {
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+			String xml = xstream.toXML(empresas);
+					
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);			
+		} else if(valor.endsWith("json")) {
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+			
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+		} else {
+			response.setContentType("application/json");
+			response.getWriter().print("{'message':'no content'}");
+		}
 	}
 }
